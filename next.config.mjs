6b90be34a8f83fb866554f8f587933f679751d2e
akshu2210 <koduruakshaya22@gmail.com>
2026-08-19 19:47:18 +0530

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/netflix-n1",
+  assetPrefix: "/netflix-n1/",
+  trailingSlash: true,
 };
 
 export default nextConfig;
