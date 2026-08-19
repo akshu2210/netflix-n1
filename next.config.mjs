ffc14cc3f60cc704e1c3391c25ca014314e6424e
akshu2210 <koduruakshaya22@gmail.com>
2026-08-19 20:15:53 +0530

    Fix Netflix N-1 landing page

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const isGitHubPages = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig = {
   output: "export",
+
+  basePath: isGitHubPages ? "/netflix-n1" : "",
+
   images: {
     unoptimized: true,
   },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
